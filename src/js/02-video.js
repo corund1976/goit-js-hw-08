d36eb96f3d55767792a9e8fdfcf758d16840f85a
Sergii Budak <corund1976@gmail.com>
2021-09-29 21:49:10 +0300

    21.49

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate', function (eventTime) {
+    const { duration, percent, seconds } = eventTime;
+
+    localStorage.setItem("videoplayer-current-time", seconds);
+});
+
+const timeToResume = localStorage.getItem("videoplayer-current-time");
+
+console.log("Продолжаю воспроизведение с ", Math.round(timeToResume), "секунды....");
+
+player.setCurrentTime(timeToResume);
 
