22e850f9527068b1cfbdbf4e1236154a643ea566
Sergii Budak <corund1976@gmail.com>
2021-09-30 21:52:49 +0300

    Final Patch

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,23 +9,10 @@ player.on('timeupdate', throttle(data => {
     localStorage.setItem("videoplayer-current-time", data.seconds)
 }, 1000));
 
-// ---------- Предыдущие этапы решения ---------
-// player.on('timeupdate', throttle(({ duration, percent, seconds }) => {
-//     localStorage.setItem("videoplayer-current-time", seconds);
-//     }, 1000));
-// player.on('timeupdate', throttle((function ({ duration, percent, seconds }) {
-//     localStorage.setItem("videoplayer-current-time", seconds);
-//     }), 1000));
-
 // ========== Вызов плеера с текущей секунды воспроизведения (из локального хранилища)
 
-let timeToResume = localStorage.getItem("videoplayer-current-time");
+let timeToResume = localStorage.getItem("videoplayer-current-time") || 0;
 
-if (!timeToResume) {
-    timeToResume = 0;
-};
-
-// console.log("Продолжаю воспроизведение с ", Math.round(timeToResume), "секунды....");
 player.setCurrentTime(timeToResume);
 
 // localStorage.removeItem("videoplayer-current-time");
