af0fc736abb66e2575c8901d77969893d4837106
Sergii Budak <corund1976@gmail.com>
2021-09-30 21:38:34 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,34 @@
 import throttle from 'lodash.throttle';
 
-// --------- Поля Inputs
-const inputEmail = document.querySelector('#inputEmail');
-const textareaMessage = document.querySelector('#textareaMessage');
+// --------- Форма и ее поля ввода
+const inputForm = document.querySelector('.feedback-form');
+const formEmail = inputForm.querySelector("input");
+const formMessage = inputForm.querySelector("textarea");
 
 // --------- Значение обьекта ввода в локальном хранилище
 let savedInputs = localStorage.getItem("feedback-form-state");
 let parsedInputs = JSON.parse(savedInputs);
 
 // ---------- Проверяем, что обьект в хранилище не пустой
-if (savedInputs) {
-// ---------Заполняем поля Inputs значениями из локального хранилища
-    inputEmail.value = parsedInputs.email;
-    textareaMessage.value = parsedInputs.message;
+if (parsedInputs) {
+// ---------Заполняем поля ввода значениями из локального хранилища
+    formEmail.value = parsedInputs.email;
+    formMessage.value = parsedInputs.message;
 }
 
-// --------- Сама форма
-const inputForm = document.querySelector('.feedback-form');
-// --------- Вешаем на нее "слушателей"
-inputForm.addEventListener("input", handleInput);
-// inputForm.addEventListener("input", throttle(handleInput, 500));
+// --------- Вешаем на форму "слушателей"
+inputForm.addEventListener("input", throttle(handleInput, 500));
 inputForm.addEventListener("submit", handleSubmit);
 
 // ---------- Колбэк для Inputs
-function handleInput(event) {
-    const { email, message } = event.currentTarget.elements;
-        
+function handleInput() {
     const data = {
-        email: email.value,
-        message: message.value
+        email: inputForm.elements.email.value,
+        message: inputForm.elements.message.value
     }
 
     // записываем "текущие значения полей формы" в "локальное хранилище"
-    // function callback () {
-        localStorage.setItem("feedback-form-state", JSON.stringify(data));
-    // };
-
-    // throttle(callback, 500);
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
 }
 
 // --------- Колбэк для Submit
@@ -48,7 +40,7 @@ function handleSubmit(event) {
     parsedInputs = JSON.parse(savedInputs);
     console.log(parsedInputs);
 
-    //очистка формы инпута при нажатии Submit
+    //очистка формы при нажатии Submit
     event.currentTarget.reset();
 
     //очистка локального хранилища при нажатии Submit
